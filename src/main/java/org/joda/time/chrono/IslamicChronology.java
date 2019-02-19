1902f19a818ec18daeadd3cd5180f849102ea8c0
sainzatoledano <sainza.toledano@udc.es>
2019-02-19 09:05:35 +0100

    Herramientas utilizadas hasta el momomento spogbugs, pit, Travis CI (para integración continua), Coveralls y JaCoCo (para covertura)

--- src/main/java/org/joda/time/chrono/IslamicChronology.java
+++ src/main/java/org/joda/time/chrono/IslamicChronology.java
@@ -361,7 +361,7 @@ public final class IslamicChronology extends BasicChronology {
 
     //-----------------------------------------------------------------------
     long getTotalMillisByYearMonth(int year, int month) {
-        if (--month % 2 == 1) {
+        if ((--month & 1) == 1) {
             month /= 2;
             return month * MILLIS_PER_MONTH_PAIR + MILLIS_PER_LONG_MONTH;
         } else {
